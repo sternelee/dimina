7f72781128a6b5428e523aa73411a22a94c1c6c7
doslin <doslino@gmail.com>
2025-05-26 20:13:19 +0800

    ci(android): streamline Android CI workflow to only run unit tests with improved caching

--- android/app/src/androidTest/java/com/didi/dimina/demo/ExampleInstrumentedTest.kt
+++ android/app/src/androidTest/java/com/didi/dimina/demo/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.didi.dimina
+package com.didi.dimina.demo
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.didi.dimina", appContext.packageName)
+        assertEquals("com.didi.dimina.demo", appContext.packageName)
     }
 }
