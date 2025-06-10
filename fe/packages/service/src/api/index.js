264e5623a47d7a308eef9dc22fcbda692db295f9
dos1in <doslino@gmail.com>
2025-06-10 15:09:45 +0800

    feat(fe,service): revert proxy api and add compatibility handling for Taro

--- fe/packages/service/src/api/index.js
+++ fe/packages/service/src/api/index.js
@@ -1,8 +1,41 @@
+import { invokeAPI } from '@/api/common'
+
 const apiInfo = import.meta.glob('./core/**/index.js', { eager: true })
-const globalApi = {}
+const api = {}
 for (const f of Object.values(apiInfo)) {
 	for (const [k, v] of Object.entries(f)) {
-		globalApi[k] = v
+		api[k] = v
 	}
 }
+const handler = {
+	get(target, prop, receiver) {
+		const origMethod = Reflect.get(target, prop, receiver)
+		
+		// API存在则直接调用，API 已具体实现
+		if (typeof origMethod === 'function') {
+			return origMethod
+		}
+		
+		// 如果是非函数属性且已存在，或者特殊处理 webpackJsonp 属性来兼容 Taro，直接返回
+		if (origMethod !== undefined || prop === 'webpackJsonp') {
+			return origMethod
+		}
+		
+		// API 不存在则返回一个函数，通过消息通道调用
+		return (...args) => {
+			return invokeAPI(prop, ...args)
+		}
+	},
+	set(target, prop, value, receiver) {
+		// 允许对target对象进行属性赋值
+		return Reflect.set(target, prop, value, receiver)
+	},
+}
+/**
+ * 外部挂载 API，内部转发不存在 API
+ * [Render]invokeAPI -> [Container]invokeAPI -> [Service]invokeAPI -> [Container]invokeAPI
+ */
+
+const globalApi = new Proxy(api, handler)
+
 export default globalApi
