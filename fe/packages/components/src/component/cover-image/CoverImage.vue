edcc94ef90d149a1677c86dad50fb567ce6cf95a
dos1in <doslino@gmail.com>
2026-05-17 17:10:19 +0800

    revert(fe): remove native overlay logic from CoverImage and CoverView components

--- fe/packages/components/src/component/cover-image/CoverImage.vue
+++ fe/packages/components/src/component/cover-image/CoverImage.vue
@@ -1,8 +1,8 @@
 <script setup>
+import { useInfo } from '@/common/events'
 // 覆盖在原生组件之上的图片视图。
 // 可覆盖的原生组件同cover-view，支持嵌套在cover-view里
 // https://developers.weixin.qq.com/miniprogram/dev/component/cover-image.html
-import { useNativeOverlayComponent } from '@/common/useNativeOverlayComponent'
 import Image from '../image/Image.vue'
 
 const props = defineProps({
@@ -10,39 +10,10 @@ const props = defineProps({
 		type: String,
 		default: '',
 	},
-	mode: {
-		type: String,
-		default: '',
-	},
 })
-const rootRef = ref()
-const { isAndroid, nativeId } = useNativeOverlayComponent({
-	type: 'native/cover-image',
-	rootRef,
-	getParams: () => ({
-		src: props.src,
-		mode: props.mode,
-	}),
-	watchSources: () => [props.src, props.mode],
-})
+useInfo()
 </script>
 
 <template>
-	<span v-if="isAndroid" :id="nativeId" ref="rootRef" v-bind="$attrs" class="dd-cover-image-native-placeholder">
-		<img :src="props.src" alt="" />
-	</span>
-	<Image v-else v-bind="$attrs" :src="props.src" :mode="props.mode" />
+	<Image v-bind="$attrs" :src="props.src" />
 </template>
-
-<style lang="scss">
-.dd-cover-image-native-placeholder {
-	display: inline-block;
-	pointer-events: none !important;
-	visibility: hidden !important;
-
-	img {
-		width: inherit;
-		height: inherit;
-	}
-}
-</style>
--- fe/packages/components/src/component/cover-view/CoverView.vue
+++ fe/packages/components/src/component/cover-view/CoverView.vue
@@ -1,33 +1,16 @@
 <script setup>
+import { useInfo } from '@/common/events'
 // 覆盖在原生组件之上的文本视图。
 // 可覆盖的原生组件包括 map、video、canvas、camera、live-player、live-pusher
 // 只支持嵌套 cover-view、cover-image，可在 cover-view 中使用 button。组件属性的长度单位默认为px
 // https://developers.weixin.qq.com/miniprogram/dev/component/cover-view.html
-import { useNativeOverlayComponent } from '@/common/useNativeOverlayComponent'
 import View from '../view/View.vue'
 
-const rootRef = ref()
-const { isAndroid, nativeId } = useNativeOverlayComponent({
-	type: 'native/cover-view',
-	rootRef,
-	getParams: () => ({
-		text: rootRef.value?.textContent?.replace(/\s+/g, ' ').trim() || '',
-	}),
-})
+useInfo()
 </script>
 
 <template>
-	<div v-if="isAndroid" :id="nativeId" ref="rootRef" v-bind="$attrs" class="dd-cover-view-native-placeholder">
-		<slot />
-	</div>
-	<View v-else v-bind="$attrs">
+	<View v-bind="$attrs">
 		<slot />
 	</View>
 </template>
-
-<style lang="scss">
-.dd-cover-view-native-placeholder {
-	pointer-events: none !important;
-	visibility: hidden !important;
-}
-</style>
