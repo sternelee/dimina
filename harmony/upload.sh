f9d90f122ac5c67718c2f7c6d4c2bdcfe3cf5bc5
dos1in <doslino@gmail.com>
2026-06-01 15:07:52 +0800

    feat(harmony): add build har command to upload script

--- harmony/upload.sh
+++ harmony/upload.sh
@@ -1,1 +1,2 @@
+hvigorw --mode module -p product=default -p module=dimina@default assembleHar --analyze=normal --parallel --incremental --daemon
 ohpm publish dimina/build/default/outputs/default/dimina.har
